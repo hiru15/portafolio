1a1f1518645a723d1abdfe27df5e5921db37152e
felipe <felipebenitez@gmail.com>
2017-12-12 22:38:25 -0300

    Inicio del Portafolio

---
+++ components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="container">
+	<nav class="navbar is-transparent is-12" role="navigation">
+	  <button class="button navbar-burger" data-target="navMenu">
+	   <span></span>
+	   <span></span>
+	   <span></span>
+	  </button>
+	   <div class="navbar-menu" id="navMenu">
+		  <div class="navbar-star">
+		  	<a class="navbar-item">
+			 Home
+		  	</a>
+		  	<a class="navbar-item">
+			 Profesional
+		  	</a>
+		  	<a class="navbar-item">
+			 Experiencia
+		  	</a>
+		  	<a class="navbar-item">
+			 Portafolio
+		  	</a>
+		  	<a class="navbar-item">
+			 Contacto
+		  	</a>
+		  </div>
+	   </div>
+	</nav>
+</div>
+</template>
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target
+        var $target = document.getElementById(target)
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active')
+        $target.classList.toggle('is-active')
+      })
+    })
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+div.container
+  Navbar
+  div.columns
+    div.column.is-10.is-offset-1.contenedor
+      div.columns
+        div.column.is-3.is-offset-2.espacio-text
+          h2.title Contacto
+          p.text I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.
+        div.column.is-4.is-offset-1.espacio 
+         div.columns
+          div.column.is-6.field
+            div.control
+              input.input(placeholder="Nombre")
+          div.column.is-6.field
+            div.control
+              input.input(placeholder="Email")
+         div.columns
+          div.column.is-12.field
+            div.control
+              input.input(placeholder="Asunto")
+         div.columns
+          div.column.is-12.field
+            div.control
+              textarea.textarea(placeholder="Mensaje")
+         div.columns
+          div.column.is-12.field
+            div.control
+              button.button.is-success.is-large Enviar
+      div.columns
+        div.column.is-5.is-offset-5
+          a.fa.fa-facebook.icon.has-text-white(href="https://www.facebook.com/ehlpiphex")
+          a.fa.fa-instagram.icon.has-text-white(href="https://www.facebook.com/ehlpiphex")
+  nuxt
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+.is-large{
+  width: 100%;
+}
+.contenedor{
+  background: #201B3F;
+  margin-top: 20px;
+}
+.espacio-text{
+  margin-top: 65px;
+}
+.espacio{
+  margin-top: 65px;
+  margin-bottom: 40px;
+}
+.title {
+  font-size: 28px;
+  color: #fff;
+}
+.text{
+  color: #fff;
+  text-align: justify;
+}
+</style>
